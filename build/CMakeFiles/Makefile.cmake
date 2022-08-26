d5a7f6775ee5839a795d33774355d59b8cbd2698
Your Name <you@example.com>
2022-08-26 15:34:09 +0900

    Make

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -44,4 +44,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/runLinkedLists.dir/DependInfo.cmake"
+  "CMakeFiles/runCircularLinkedLists.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/runCircularLinkedLists.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runCircularLinkedLists.dir/CircularLinkedList.c.o"
+  "CMakeFiles/runCircularLinkedLists.dir/CircularLinkedList.c.o.d"
+  "CMakeFiles/runCircularLinkedLists.dir/ex4_3.c.o"
+  "CMakeFiles/runCircularLinkedLists.dir/ex4_3.c.o.d"
+  "runCircularLinkedLists"
+  "runCircularLinkedLists.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/runCircularLinkedLists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
