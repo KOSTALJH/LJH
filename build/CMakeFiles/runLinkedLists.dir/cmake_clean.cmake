499eef48d2c5145bf3cedf3722d5ca9992c12f4a
Your Name <you@example.com>
2022-08-26 11:04:44 +0900

    ignore build config

---
+++ build/CMakeFiles/runLinkedLists.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runLinkedLists.dir/LinkedList.c.o"
+  "CMakeFiles/runLinkedLists.dir/LinkedList.c.o.d"
+  "CMakeFiles/runLinkedLists.dir/ex4_2.c.o"
+  "CMakeFiles/runLinkedLists.dir/ex4_2.c.o.d"
+  "runLinkedLists"
+  "runLinkedLists.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/runLinkedLists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
